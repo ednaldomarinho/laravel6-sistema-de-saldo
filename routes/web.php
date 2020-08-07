d0950d3b25016b39c1b7bde9cef92b549c808be7
ednaldomarinho <ednaldosmarinho@gmail.com>
2020-08-07 23:04:51 +0000

    Lesson 12 - Organize the Application

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
+Route::group(['middleware' => ['auth'], 'namespace' => 'Admin'],function(){
+    Route::get('admin', 'AdminController@index')->name('admin.home');
 });
 
-Auth::routes();
 
-Route::get('/home', function() {
-    return view('home');
-})->name('home')->middleware('auth');
+
+Route::get('/', 'Site\SiteController@index')->name('home');
+
+Auth::routes();
